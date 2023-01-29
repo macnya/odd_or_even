2c81debb9bf093f5877c12117f2cacd12f0fd7e3
macnya <61224738+macnya@users.noreply.github.com>
2023-01-29 18:13:16 +0300

    Add files via upload

---
+++ odd_or_even.py
@@ -0,0 +1,8 @@
+print('Welcome!')
+
+num = int(input('Please enter a number between 1 and 1000: '))
+
+if (num % 2) == 0:
+    print('The number you entered is even.')
+else:
+    print('The number you entered is odd.')
